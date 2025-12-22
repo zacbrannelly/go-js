100773957c5cff6c6d6701deaa161f04f1784e8d
Zac Brannelly <zac.brannelly@gmail.com>
2025-12-22 11:02:18 +1100

    Support `Array.prototype.copyWithin`

--- pkg/lib-js/runtime/array_prototype.go
+++ pkg/lib-js/runtime/array_prototype.go
@@ -1,5 +1,9 @@
 package runtime
 
+import (
+	"math"
+)
+
 var LengthString = NewStringValue("length")
 
 func NewArrayPrototype(runtime *Runtime) ObjectInterface {
@@ -14,6 +18,9 @@ func NewArrayPrototype(runtime *Runtime) ObjectInterface {
 
 	// Array.prototype[%Symbol.iterator%]
 	DefineBuiltinSymbolFunction(runtime, obj, runtime.SymbolIterator, ArrayPrototypeValues, 0)
+
+	// Array.prototype.copyWithin
+	DefineBuiltinFunction(runtime, obj, "copyWithin", ArrayPrototypeCopyWithin, 2)
 
 	// TODO: Implement other methods.
 
@@ -98,4 +105,141 @@ func LengthOfArrayLike(runtime *Runtime, object ObjectInterface) *Completion {
 	len := lenCompletion.Value.(*JavaScriptValue)
 	lenCompletion = ToLength(len)
 	return lenCompletion
+}
+
+func ArrayPrototypeCopyWithin(
+	runtime *Runtime,
+	function *FunctionObject,
+	thisArg *JavaScriptValue,
+	arguments []*JavaScriptValue,
+	newTarget *JavaScriptValue,
+) *Completion {
+	for idx := range 3 {
+		if idx >= len(arguments) {
+			arguments = append(arguments, NewUndefinedValue())
+		}
+	}
+
+	targetArg := arguments[0]
+	startArg := arguments[1]
+	endArg := arguments[2]
+
+	completion := ToObject(thisArg)
+	if completion.Type != Normal {
+		return completion
+	}
+
+	objectVal := completion.Value.(*JavaScriptValue)
+	object := objectVal.Value.(ObjectInterface)
+
+	completion = LengthOfArrayLike(runtime, object)
+	if completion.Type != Normal {
+		return completion
+	}
+
+	len := completion.Value.(*JavaScriptValue).Value.(*Number).Value
+
+	completion = ToIntegerOrInfinity(targetArg)
+	if completion.Type != Normal {
+		return completion
+	}
+
+	relativeTarget := completion.Value.(*JavaScriptValue)
+	to := ToRelativeIndex(relativeTarget, len)
+
+	completion = ToIntegerOrInfinity(startArg)
+	if completion.Type != Normal {
+		return completion
+	}
+	relativeStart := completion.Value.(*JavaScriptValue)
+	from := ToRelativeIndex(relativeStart, len)
+
+	var relativeEnd *JavaScriptValue
+	if endArg.Type == TypeUndefined {
+		relativeEnd = NewNumberValue(len, false)
+	} else {
+		completion = ToIntegerOrInfinity(endArg)
+		if completion.Type != Normal {
+			return completion
+		}
+		relativeEnd = completion.Value.(*JavaScriptValue)
+	}
+	final := ToRelativeIndex(relativeEnd, len)
+
+	count := math.Min(final-from, len-to)
+
+	var direction float64
+	if from < to && to < from+count {
+		direction = -1
+		from = from + count - 1
+		to = to + count - 1
+	} else {
+		direction = 1
+	}
+
+	for count > 0 {
+		completion = ToString(NewNumberValue(float64(from), false))
+		if completion.Type != Normal {
+			panic("Assert failed: ToString threw an unexpected error.")
+		}
+		fromKey := completion.Value.(*JavaScriptValue)
+
+		completion = ToString(NewNumberValue(float64(to), false))
+		if completion.Type != Normal {
+			panic("Assert failed: ToString threw an unexpected error.")
+		}
+		toKey := completion.Value.(*JavaScriptValue)
+
+		completion = object.HasProperty(fromKey)
+		if completion.Type != Normal {
+			return completion
+		}
+
+		hasFrom := completion.Value.(*JavaScriptValue).Value.(*Boolean).Value
+		if hasFrom {
+			completion = object.Get(runtime, fromKey, objectVal)
+			if completion.Type != Normal {
+				return completion
+			}
+
+			fromValue := completion.Value.(*JavaScriptValue)
+
+			completion = object.Set(runtime, toKey, fromValue, objectVal)
+			if completion.Type != Normal {
+				return completion
+			}
+			if !completion.Value.(*JavaScriptValue).Value.(*Boolean).Value {
+				return NewThrowCompletion(NewTypeError("Failed to set property."))
+			}
+		} else {
+			completion = object.Delete(toKey)
+			if completion.Type != Normal {
+				return completion
+			}
+			if !completion.Value.(*JavaScriptValue).Value.(*Boolean).Value {
+				return NewThrowCompletion(NewTypeError("Failed to delete property."))
+			}
+		}
+
+		from += direction
+		to += direction
+		count -= 1
+	}
+
+	return NewNormalCompletion(objectVal)
+}
+
+func ToRelativeIndex(value *JavaScriptValue, length float64) float64 {
+	if value.Type != TypeNumber {
+		panic("Assert failed: ToRelativeIndex value is not a number.")
+	}
+
+	relativeIndex := value.Value.(*Number).Value
+	if relativeIndex == math.Inf(-1) {
+		return 0
+	} else if relativeIndex < 0 {
+		return math.Max(length+relativeIndex, 0)
+	} else {
+		return math.Min(relativeIndex, length)
+	}
 }
